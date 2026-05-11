c1ad312343393897bdcbd12f84fe25d3be494482
Masayoshi AMAMOTO <k22rs004@st.kyusan-u.ac.jp>
2026-05-11 12:42:08 +0900

    変数と条件分岐を追加

--- hello.rb
+++ hello.rb
@@ -1,3 +1,29 @@
 #! /usr/bin/env ruby
 
 puts "Hello, World!"
+a = -1
+s = "Ruby"
+d = 3.14
+puts "a:"+a.to_s
+puts "s:"+s
+puts "d:"+d.to_s
+
+if a > 5
+    puts "Large"
+elsif a < 0
+    puts "Negative"
+else
+    puts "Little"
+end
+
+x = 5
+while x > 0
+    print "#"
+    x-=1
+end
+puts
+
+5.times do |i|
+    print i.to_s + "#"
+end
+puts
