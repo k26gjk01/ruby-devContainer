4dbdbb10f1a4ac44e97729f57bc4b3b5e08a5885
Masayoshi AMAMOTO <k22rs004@st.kyusan-u.ac.jp>
2026-06-15 12:34:36 +0900

    HTTP/1.0に
    HTTP/1.0対応に変更

--- client1.rb
+++ client1.rb
@@ -4,12 +4,17 @@ require 'socket'
 host = 'www.is.kyusan-u.ac.jp'
 port = 'http'
 path = '/~toshi/'
+version = 'HTTP/1.0'
 
 sock = TCPSocket.new host, port
-cmd = 'GET ' + path + "\r\n"
+cmd = 'GET ' + path + ' ' + version + "\r\n\r\n"
 pp cmd
 sock.print cmd
 
+is_body = false
 while line = sock.gets
-  puts line
+  puts line if is_body
+  if line == "\r\n"
+    is_body = true
+  end
 end
