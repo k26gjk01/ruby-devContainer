94f276c19536e70f6321d5da5192e6f1ec6f7642
Masayoshi AMAMOTO <k22rs004@st.kyusan-u.ac.jp>
2026-06-22 12:01:31 +0900

    Connection: closeを追加
    Connection: closeで早くプログラムを終了するようにした

--- client1.rb
+++ client1.rb
@@ -1,20 +1,21 @@
 #client1.rb
 require 'socket'
 
+
+
 host = 'www.is.kyusan-u.ac.jp'
 port = 'http'
 path = '/~toshi/'
 version = 'HTTP/1.1'
 
 sock = TCPSocket.new host, port
-cmd = 'GET ' + path + ' ' + version + "\r\n"
-cmd2 = 'Host: ' + host + "\r\n"
-cmd_end = "\r\n"
+#cmd = 'GET ' + path + ' ' + version + "\r\n" + 'Host: ' + host + "\r\n\r\n"
+cmd = "GET #{path} #{version}\r\n"
+cmd += "Host: #{host}\r\n"
+cmd += "Connection: close\r\n\r\n"
 
 pp cmd
 sock.print cmd
-sock.print cmd2
-sock.print cmd_end
 
 is_body = false
 while line = sock.gets
