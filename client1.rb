a36dade43dcb5dfceb0b6538efd31b4615b28d01
Masayoshi AMAMOTO <k22rs004@st.kyusan-u.ac.jp>
2026-06-22 11:41:24 +0900

    HTTP/1.1に対応（複数行ver）

--- client1.rb
+++ client1.rb
@@ -4,12 +4,17 @@ require 'socket'
 host = 'www.is.kyusan-u.ac.jp'
 port = 'http'
 path = '/~toshi/'
-version = 'HTTP/1.0'
+version = 'HTTP/1.1'
 
 sock = TCPSocket.new host, port
-cmd = 'GET ' + path + ' ' + version + "\r\n\r\n"
+cmd = 'GET ' + path + ' ' + version + "\r\n"
+cmd2 = 'Host: ' + host + "\r\n"
+cmd_end = "\r\n"
+
 pp cmd
 sock.print cmd
+sock.print cmd2
+sock.print cmd_end
 
 is_body = false
 while line = sock.gets
