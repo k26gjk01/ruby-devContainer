2e7e706fd4a80abcc3743b36c33e3610ff1175d0
Masayoshi AMAMOTO <k22rs004@st.kyusan-u.ac.jp>
2026-06-29 12:04:00 +0900

    server1.rbを作成
    とりあえず動くサーバ

---
+++ server1.rb
@@ -0,0 +1,20 @@
+#! /usr/local/bin/ruby
+# server1.rb
+
+require 'socket'
+
+gs = TCPServer.open 'http'
+
+loop do
+  pp "start accept"
+  s = gs.accept
+  while line = s.gets
+    pp line
+    s.puts line
+    break if line == "\r\n"
+  end
+  s.close
+  sleep 10
+end
+
+
