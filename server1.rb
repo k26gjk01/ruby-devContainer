d09b913e501bc4eb3c40e10a42992990426b8d11
Masayoshi AMAMOTO <k22rs004@st.kyusan-u.ac.jp>
2026-07-06 12:04:10 +0900

    クライアントの挙動を見る

---
+++ server1.rb
@@ -0,0 +1,29 @@
+#! /usr/local/bin/ruby
+# server1.rb
+
+require 'socket'
+
+def server s
+  cmd,path,ver = s.gets.split " "
+  pp [cmd,path,ver]
+  s.puts cmd
+  while line = s.gets
+    pp line
+    s.puts line
+    break if line == "\r\n"
+  end
+  s.close
+end
+
+gs = TCPServer.open 'http'
+
+loop do
+  pp "start accept"
+  s = gs.accept
+  Thread.new do
+    server s
+    sleep 10
+  end
+end
+
+
