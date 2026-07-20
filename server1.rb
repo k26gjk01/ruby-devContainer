945d72f728fa99743d6911bb94656422ebfb4a2e
Masayoshi AMAMOTO <145627761+k26gjk01@users.noreply.github.com>
2026-07-20 10:58:33 +0900

    Update server1.rb

--- server1.rb
+++ server1.rb
@@ -9,33 +9,54 @@ require 'socket'
 
 def server s
   cmd, path, ver = s.gets.split " "
+  # HTTP/1.0 として正しく返答
   # 1行目 HTTP/1.0 200 OK
   # 2行目 Content-Type: text/html
   # 3行目 空行
   # 4行目 コンテンツ
   # 最後 ソケットをクローズ
+
   if path == "/"
     s.print "HTTP/1.0 200 OK\r\n"
     s.print "Content-Type: text/html\r\n"
     s.print "\r\n"
     pp "INDEX"
     s.puts "<h1>index</h1>"
+  elsif path == "/api/now"
+    s.print "HTTP/1.0 200 OK\r\n"
+    s.print "Content-Type: application/json; charset=UTF-8\r\n"
+    s.print "\r\n"
+    s.puts "{"
+    s.puts '"time": "' + "#{Time.now}" + '"'
+    s.puts "}"
   else
-    pp "OTHER"
-    s.puts "other"
+    file = path.slice 1..-1
+    if File.exist? file
+      s.print "HTTP/1.0 200 OK\r\n"
+      s.print "Content-Type: text/plain; charset=UTF-8\r\n"
+      s.print "\r\n"
+      File.open file, "r" do |f|
+        while line = f.gets
+          s.puts line
+        end
+      end
+    else
+      s.print "HTTP/1.0 404 Not Found\r\n"
+      s.print "Content-Type: text/html; charset=UTF-8\r\n"
+      s.print "\r\n" 
+      s.puts "<h1>404 Not Found</h1>"      
+      s.puts "<p> File: " + path + "</p>"
+    end
   end
   s.close
 end
 
 gs = TCPServer.open 'http'
-
 loop do
   pp "start accept"
   s = gs.accept
   Thread.new do
     server s
-    sleep 10
   end
 end
-
 
